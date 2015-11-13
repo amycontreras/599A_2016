09b147ef637d3d03dd7182b68a67b4b703123674
Amy Contreras <amycontreras9@yahoo.com>
2015-11-13 13:16:25 -0800

    updated drive code with variables driveY and driveX to vary speed, DEADZONE is still NOT a variable at 10. the code works gr8git add . !

--- Drive.h
+++ Drive.h
@@ -1,43 +1,43 @@
 #include "Macros.h"
-
+/*
 void updateDriveMotors(int left,int right)
 {
 	motor[front_left_drive_motor] = left;
 	motor[back_left_drive_motor] = left;
 	motor[front_right_drive_motor] = right;
 	motor[back_right_drive_motor] = right;
-}
+}*/
 
 
-void ok()
+void drive()
 {
 	if(vexRT[Ch3] > 10)
 	{
-		motor[front_left_drive_motor] = 127;
-		motor[back_left_drive_motor] = 127;
-		motor[front_right_drive_motor] = 127;
-		motor[back_right_drive_motor] = 127;
+		motor[front_left_drive_motor] = driveY;
+		motor[back_left_drive_motor] = driveY;
+		motor[front_right_drive_motor] = driveY;
+		motor[back_right_drive_motor] = driveY;
 	}
 	else if(vexRT[Ch3] < -10)
 	{
-		motor[front_left_drive_motor] = -127;
-		motor[back_left_drive_motor] = -127;
-		motor[front_right_drive_motor] = -127;
-		motor[back_right_drive_motor] = -127;
+		motor[front_left_drive_motor] = -driveY;
+		motor[back_left_drive_motor] = -driveY;
+		motor[front_right_drive_motor] = -driveY;
+		motor[back_right_drive_motor] = -driveY;
 	}
 	else if(vexRT[Ch1] > 10)
 	{
-		motor[front_left_drive_motor] = 127;
-		motor[back_left_drive_motor] = 127;
-		motor[front_right_drive_motor] = -127;
-		motor[back_right_drive_motor] = -127;
+		motor[front_left_drive_motor] = driveX;
+		motor[back_left_drive_motor] = driveX;
+		motor[front_right_drive_motor] = -driveX;
+		motor[back_right_drive_motor] = -driveX;
 	}
 	else if(vexRT[Ch1] < -10)
 	{
-		motor[front_left_drive_motor] = -127;
-		motor[back_left_drive_motor] = -127;
-		motor[front_right_drive_motor] = 127;
-		motor[back_right_drive_motor] = 127;
+		motor[front_left_drive_motor] = -driveX;
+		motor[back_left_drive_motor] = -driveX;
+		motor[front_right_drive_motor] = driveX;
+		motor[back_right_drive_motor] = driveX;
 	}
 	else if(vexRT[Btn8L] == 1)
 	{
@@ -65,5 +65,5 @@ void ok()
 
 void updateDrive()
 {
-	ok();
+	drive();
 }
