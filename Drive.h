168d07549ab2c5411c1193f1a8423275bbf62ac2
Amy Contreras <amycontreras9@yahoo.com>
2015-11-19 14:54:15 -0800

    removed updateDriveMotors function, all morots are written out. DEADZONE is now re-implemented, using the variable works.

--- Drive.h
+++ Drive.h
@@ -1,21 +1,11 @@
 #include "Macros.h"
 
-// This sets the parameters left and right to all four drive
-// motors. This is then used as a shortcut in the drive function
-// in order to condense lines of code.
-void updateDriveMotors(int left,int right)
-{
-	motor[front_left_drive_motor] = left;
-	motor[back_left_drive_motor] = left;
-	motor[front_right_drive_motor] = right;
-	motor[back_right_drive_motor] = right;
-}
-
 // This is basic drive code. You check to see whether or not
-// the joystick channel is greater than the DEADZONE. When
+// the joystick channel is greater than the DEADZONE, a value
+// of 10 (set in the Macros.h file) that prevents random
+// movements upon accidentally jolting the controller. When
 // testing this robot, replacing each item with shortened
-// variables did not work. So here is everything written out
-// for now, as of 11/16/2015.
+// variables did not work.
 void drive()
 {
 	int fwd;
@@ -24,37 +14,23 @@ void drive()
 	spin = vexRT[Ch1];
 	fwd = vexRT[Ch3];
 
-	if(abs(vexRT[Ch3]) > 10)
+	if(abs(vexRT[Ch3]) > DEADZONE)
 	{
 		motor[front_left_drive_motor] = fwd;
 		motor[back_left_drive_motor] = fwd;
 		motor[front_right_drive_motor] = fwd;
 		motor[back_right_drive_motor] = fwd;
-	}/*
-	else if(vexRT[Ch3] < -10)
-	{
-		motor[front_left_drive_motor] = -fwd;
-		motor[back_left_drive_motor] = -fwd;
-		motor[front_right_drive_motor] = -fwd;
-		motor[back_right_drive_motor] = -fwd;
-	}*/
-	else if(abs(vexRT[Ch1]) > 10)
+	}
+	else if(abs(vexRT[Ch1]) > DEADZONE)
 	{
 		motor[front_left_drive_motor] = spin;
 		motor[back_left_drive_motor] = spin;
 		motor[front_right_drive_motor] = -spin;
 		motor[back_right_drive_motor] = -spin;
-	}/*
-	else if(vexRT[Ch1] < -10)
-	{
-		motor[front_left_drive_motor] = -spin;
-		motor[back_left_drive_motor] = -spin;
-		motor[front_right_drive_motor] = spin;
-		motor[back_right_drive_motor] = spin;
-	}*/
+
+	}
 // This is strafe code...with buttons. It lets the robot
 // strafe at full speed left or right if a button is pressed.
-// Kind of counter-intuitive...I'll change it soon. Anyway.
 // Certain motors are reversed in order for one pair of wheels
 // to be facing inwards with the other outwards. Drawing this
 // out really helps the logic.
