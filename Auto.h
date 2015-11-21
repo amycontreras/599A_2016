342489a9ddcccacd40f68b93b915fb9142e25395
Amy Contreras <amycontreras9@yahoo.com>
2015-11-20 22:25:04 -0800

    Wow! Working autonomous code. Drives fwd, then rev, then shoots 3 balls, and drives fwd. Works well based on placement on the field. Aka Brian is the determiner of our fate. #firstauto #cool?

--- Auto.h
+++ Auto.h
@@ -2,15 +2,43 @@
 
 void autoTest()
 {
-	updateDriveMotors(60, 60);
-	wait1Msec(2000);
-	updateDriveMotors(0, 0);
-	/*
-	updateFlywheelMotors(127);
-	wait1Msec(3000);
-	updateIntakeMotors(127);
-	wait1Msec(4000);
-	updateFlywheelMotors(0);
-	updateIntakeMotors(0);  */
+	updateFlywheelMotors(60);
+	updateDriveMotors(-127, -127);
+	wait1Msec(5000);
+/*
+	motor[front_left_drive_motor] = -63;
+	motor[back_left_drive_motor] = 63;
+	motor[front_right_drive_motor] = 63;
+	motor[back_right_drive_motor] = -63;
 
+	wait1Msec(300);
+
+	motor[front_left_drive_motor] = 0;
+	motor[back_left_drive_motor] = 0;
+	motor[front_right_drive_motor] = 0;
+	motor[back_right_drive_motor] = 0;
+*/
+	updateDriveMotors(127, 127);
+	wait1Msec(400);
+	updateDriveMotors(0, 0);
+	// ball one
+	updateIntakeMotors(127);
+	wait1Msec(200);
+	updateIntakeMotors(0);
+	wait1Msec(200);
+	// ball two
+	updateIntakeMotors(127);
+	wait1Msec(1500);
+	updateIntakeMotors(0);
+	wait1Msec(2200);
+	// ball three
+	updateIntakeMotors(127);
+	wait1Msec(1500);
+	updateIntakeMotors(0);
+	wait1Msec(2500);
+	// drive fwd
+	updateDriveMotors(-127, -127);
+	wait1Msec(400);
+	updateDriveMotors(0, 0);
+	updateFlywheelMotors(0);
 }
